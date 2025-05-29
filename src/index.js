3cd218fd3241cacb9f4f86fe1f259ecc797dc1ba
Cryptomonk1000 <117469131+Cryptomonk1000@users.noreply.github.com>
2025-05-29 09:31:56 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MLBDashboard from "./MLBDashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MLBDashboard />);
